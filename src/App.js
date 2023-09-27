12a16d89ff400e58a9180b0112aa7df138217790
muskan1024 <shaikhmuskan1024@gmail.com>
2023-09-27 18:42:35 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import React from "react";
 function App() {
   return (
     <div>
-      Phone Directory
+      <div className="header">Phone Directory</div>
+      <button>Add</button>
+      <div>
+        <span>Name</span>
+        <br />
+        <span>Phone</span>
+      </div>
+      {/* <label htmlFor="name" >Name: </label>
+      <input id="name" type="text" placeholder="Type Here" defaultValue="shilpa"></input> */}
     </div>
   );
 }
