43708743aa6427bfc40e9b87dcab99cd2f1ac2a5
muskan1024 <shaikhmuskan1024@gmail.com>
2023-09-29 21:13:14 +0530

    Added state in ‘AddSubscriber’ and ‘App’ components

--- src/App.js
+++ src/App.js
@@ -4,24 +4,31 @@ import Header from "./Header";
 import './App.css'
 import { render } from "@testing-library/react";
 
+// let subscriber = [
+//   {
+//     id:1,
+//     name: "Shilpa",
+//     phone: "88888888"
+//   },
+//   {
+//     id:2,
+//     name : "Srishti",
+//     phone:"77777777"
+//   }
+// ]
 class App extends Component {
-  deleteHandler(){
-    alert("Delete Clicked")
+  // deleteHandler(){
+  //   alert("Delete Clicked")
+  // }
+
+  constructor()
+  {
+    super();
+    this.state = {
+      subscriberListToShow : []
+    }
   }
   render(){
-    let subscriber = [
-      {
-        id:1,
-        name: "Shilpa",
-        phone: "88888888"
-      },
-      {
-        id:2,
-        name : "Srishti",
-        phone:"77777777"
-      }
-    ]
-  
   return (
     <div>
       <Header heading="Phone Directory"/>
@@ -33,7 +40,7 @@ class App extends Component {
           <span className="grid-item phone-heading">Phone</span>
         </div>
         {
-          subscriber.map(sub => {
+          this.state.subscriberListToShow.map(sub => {
               return <div key={sub.id} className="grid-container">
               <span className="grid-item">{sub.name}</span>
               <span className="grid-item">{sub.phone}</span>
